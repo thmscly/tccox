f5a8e8d870440b0ac31e6d8500ceb0187b1452b9
T.C. Cox <thmscly@gmail.com>
2023-04-06 11:55:48 -0400

    initial commit | some tutorial stuff

--- src/pages/posts/first-post.js
+++ src/pages/posts/first-post.js
@@ -1,3 +1,24 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../../components/layout';
+
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+    return (
+        <Layout>
+            <Head>
+                <title>First Blog Post</title>
+            </Head>
+            <Image
+                src="/images/profile.jpg" // Route of the image file
+                height={144} // Desired size with correct aspect ratio
+                width={144} // Desired size with correct aspect ratio
+                alt="Your Name"
+            />
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </Layout>
+    );
 }
