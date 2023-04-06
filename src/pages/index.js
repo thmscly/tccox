3ad7307b0a26178a630971fac9a845851cc16a2d
T.C. Cox <thmscly@gmail.com>
2023-04-06 17:27:11 -0400

    Added my content to Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '@/lib/posts';
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -26,17 +27,44 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
 
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
+        <div className={styles.grid}>
+          <Link href="about" className={styles.card}>
+            <h2>
+              About <span>-&gt;</span>
+            </h2>
+            <p>
+              Find in-depth information about Next.js features and&nbsp;API.
+            </p>
+          </Link>
+
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              My Work <span>-&gt;</span>
+            </h2>
+            <p>
+              Take a look at muy projects and some of what&nbsp;I can do!
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Contact <span>-&gt;</span>
+            </h2>
+            <p>
+              Reach out and touch me...on social media.
+            </p>
+          </a>
+        </div>
 
       </section>
     </Layout>
