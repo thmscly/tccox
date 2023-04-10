6bd6beba469a40f0f8bae83d9cde26372fd85d6b
T.C. Cox <thmscly@gmail.com>
2023-04-10 15:52:37 -0400

    moved stack list higher on About

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,23 +25,22 @@ export default function About() {
                     <p>I&apos;m a new dad who&apos;s just finished <em> Meta&apos;s <a href="https://www.coursera.org/professional-certificates/meta-front-end-developer?" target="_blank" rel="noopener noreferrer">Front-End Developer Professional Certification</a></em>.</p>
                     <p>I&apos;m an Eagle Scout, a drummer, a movie-lover, and a new dad.</p>
                     <p>I have a BS in Psychology from the University of Tennessee at Chattanooga. I&apos;m currently working in healthcare as a pediatric Certified Medical Assistant.</p>
+                    <div>
+                        <h3 className={utilStyles.headingLg}>My Stack:</h3>
+                        <ul className={utilStyles.list}>
+                            {techs.map((x, index) => {
+                                return <li key={index} className={utilStyles.listItem}>{x}</li>
+                            })}
+                        </ul>
+                    </div>
                     <p>I&apos;ve been working over the past year to become a <em>Software Engineer</em>, and have learned a ton, but as anyone will tell you, have an infiinity left to learn.</p>
-
-
                     <p>I&apos;ve always had a deep interest in creative writing, writing and playing music, creative outlets, as well as how things work - including people. So, with wide eyes, a desire to help, and a healthy amount of naivete, I figured early on I would become a psychologist.</p>
                     <p>After 4 years of psychology, I decided I did not, in fact, want to be a psychologist.</p>
 
                     <p>The course was React-heavy and taught me loads about not only React and state-management, but also client-side development, best practices, version control, and even computational theory, such as Time Complexity and circuitry.</p>
                 </div>
                 {/* comfortable Techs */}
-                <div>
-                    <h3 className={utilStyles.headingLg}>My Stack:</h3>
-                    <ul className={utilStyles.list}>
-                        {techs.map((x, index) => {
-                            return <li key={index} className={utilStyles.listItem}>{x}</li>
-                        })}
-                    </ul>
-                </div>
+
             </span>
 
         </Layout>
