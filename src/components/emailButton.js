d3847e9f70d9ccf2b003f2a7bde78403776985cd
T.C. Cox <thmscly@gmail.com>
2023-04-11 16:27:46 -0400

    restyled email button w icon

--- src/components/emailButton.js
+++ src/components/emailButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import layoutStyles from './layout.module.css';
+import EmailIcon from '@mui/icons-material/Email';
 
 function EmailButton() {
     return (
         <span className={layoutStyles.emailButton}>
-            <a href='mailto:thmscly@gmail.com'> <button>Reach Out!</button></a>
+            <a href='mailto:thmscly@gmail.com'> <button><EmailIcon sx={{ color: 'white' }} fontSize='large' /></button></a>
         </span>
     )
 }
