437943be8a2db8599b8cea1a1378b3d8df639bbb
T.C. Cox <thmscly@gmail.com>
2023-04-11 16:52:44 -0400

    updated meta in head

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -13,17 +13,11 @@ export default function Layout({ children, home, pageName }) {
         <div className={styles.container}>
             <NavBar />
             <Head>
-                <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
-                    content="Learn how to build a personal website using Next.js"
+                    content="A React Portfolio Site for Fron-End Engineer T.C. Cox"
                 />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle,
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
+
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
