693b0ea2f44e89a96753c3b8124bbaae451dee9d
T.C. Cox <thmscly@gmail.com>
2023-04-18 13:50:26 -0400

    issue #18: changed z index of navbar to 1

--- src/components/emailButton.js
+++ src/components/emailButton.js
@@ -5,7 +5,7 @@ import EmailIcon from '@mui/icons-material/Email';
 function EmailButton() {
     return (
         <span className={layoutStyles.emailButton}>
-            <a href='mailto:thmscly@gmail.com'> <button><EmailIcon sx={{ color: 'white' }} fontSize='large' /></button></a>
+            <a href='mailto:thmscly@gmail.com'> <button><EmailIcon className={layoutStyles.emailBtnIcon} /></button></a>
         </span>
     )
 }
