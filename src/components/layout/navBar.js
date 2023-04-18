7198201cc1f856ada982b35c0eb18d1f6607d3bf
T.C. Cox <thmscly@gmail.com>
2023-04-18 16:12:53 -0400

    added logo to navBar/styled

--- src/components/layout/navBar.js
+++ src/components/layout/navBar.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Link from 'next/link';
 import utilStyles from '../../styles/utils.module.css';
 import EmailButton from '../emailButton';
+import Logo from '../logo';
 
 
 function NavBar() {
     return (
         <>
             <nav className={utilStyles.navBar}>
-                <Link href="/">Home</Link>
+
+                <Link href="/"><Logo dimension={72} /></Link>
                 <Link href="/about">About</Link>
                 <Link href="/projects">Projects</Link>
                 <Link href="/contact">Contact</Link>
